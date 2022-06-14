797da19e11ae09a6bda0d852cd2a641ac40ffc03
rita <maria.rita27012@gmail.com>
2022-06-14 11:46:56 -0300

    commit js

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ const JOGADOR_O = "O";
 
 const COMBINACOES = [
     [0,1,2],
+    [3,4,5],
+    [6,7,8], 
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
 ]
 
 document.addEventListener("click", (event) => {
@@ -18,11 +25,20 @@ function jogar(id) {
     const celula = document.getElementById(id);
     turno = checarTurno ? JOGADOR_X : JOGADOR_O;
     celula.textContent = turno;
+    celula.classList.add(turno);
     checarTurno = !checarTurno;
     checarVencedeor(turno);
 }
 
 
 function checarVencedeor(turno) {
+    const vencedor = COMBINACOES.some((comb) => {
+        return comb.every((index) => {
+            return celulas(index).classList.contains(turnos);
+        })
+    });
 
+    if (vencedor){
+        encerraJogo(turno);
+    }
 }
